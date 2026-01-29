35d52893b93c48946e7bd3fd35fdc5ed668ff0ff
mareksimcisko <marek.simcisko@gmail.com>
2026-01-29 12:41:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let vek = 16;
+const PI = 3.14;
+let meno = "Janko";
+
+console.log(vek,meno,PI);
+console.log("Ahoj svet " + vek);
+console.log(vek + vek + PI)
+
+let vek2 = 55;
+
+
+if (true) {
+    let cislo = 5;
+    
+}
+
+let x = y * 0.8 + z;
+
+// Calculate final price with 20% tax
+
+let finalPrice = price * 0.8 + tax;
